d5f9039b9ea04d5ba99ab372f5f7077c7c2f7c72
k1LoW <k1lowxb@gmail.com>
2014-03-08 15:24:44 +0900

    Update Package-Requires

--- cake2.el
+++ cake2.el
@@ -20,7 +20,7 @@
 ;; Version: 1.1.0
 ;; Author: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
 ;; URL: http://code.101000lab.org
-;; Package-Requires: ((cake-infrector "1.0.1"))
+;; Package-Requires: ((cake-infrector "1.0.1") (historyf "0.0.8") (anything "1.3.9"))
 
 ;;; Install
 ;; Put this file into load-path'ed directory, and byte compile it if
@@ -1606,8 +1606,6 @@
         (expand-file-name (buffer-file-name)))
       )))
 
-;; mode provide
 (provide 'cake2)
-
-;;; end
 ;;; cake2.el ends here
+
