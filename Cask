ecbc94dde02e8d872ea9a3c1bab324145d6a9668
k1LoW <k1lowxb@gmail.com>
2014-04-08 08:24:28 +0900

    Update test envrionment

---
+++ Cask
@@ -0,0 +1,9 @@
+(source gnu)
+(source melpa)
+
+(package-file "cake2.el")
+
+(development
+ (depends-on "cake-inflector")
+ (depends-on "historyf")
+ (depends-on "anything"))
--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-EMACS=emacs
+if [ -z "$EMACS" ] ; then
+    EMACS=emacs
+fi
 OUTPUT=/tmp/.el-expectations
-$EMACS -q --no-site-file --batch  -L . -L t/extlib/  -l el-expectations -l cake2 -f batch-expectations $OUTPUT ./cake2.el
+cask exec $EMACS -q --no-site-file --batch  -L . -L t/extlib/ -l el-expectations -l cake2 -f batch-expectations $OUTPUT ./cake2.el
 ret=$?
 cat $OUTPUT
 rm $OUTPUT
